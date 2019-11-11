1830a8cff3541f20d8ec9563fcf1aae5796e2ead
Edvin Dičkanec <edvinas.dichkanec@gmail.com>
2019-11-11 23:20:38 +0200

    Facade design pattern

---
+++ Play3_Client_Side/DessignPatterns/Facade/GunFacade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play3_Client_Side.DessignPatterns.Facade
+{
+    class GunFacade
+    {
+        private readonly GunLowerRail glr;
+        private readonly GunUpperRail gur;
+        private readonly GunMuzzle gmuz;
+        private readonly GunMagazines gmag;
+
+        public GunFacade()
+        {
+            glr = new GunLowerRail();
+            gur = new GunUpperRail();
+            gmuz = new GunMuzzle();
+            gmag = new GunMagazines();
+        }
+
+        public void CreateCompleteGun()
+        {
+            Console.WriteLine("---------   Assembling a gun   ---------");
+            glr.setLowerRail();
+            gur.setUpperRail();
+            gmuz.setGunMuzzle();
+            gmag.setGunMagazine();
+
+            Console.WriteLine("---------   Gun assembly complete   ---------");
+        }
+    }
+}
--- Play3_Client_Side/Program.cs
+++ Play3_Client_Side/Program.cs
@@ -17,6 +17,10 @@ namespace Play3_Client_Side
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GameWindow());
+
+            var facade = new Play3_Client_Side.DessignPatterns.Facade.GunFacade();
+            facade.CreateCompleteGun();
+            Console.ReadLine();
         }
     }
 }
