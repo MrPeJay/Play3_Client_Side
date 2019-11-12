52f33f27210a7b47318e7c5c684b43e8f84abf1a
Edvin Dičkanec <edvinas.dichkanec@gmail.com>
2019-11-12 09:04:25 +0200

    Facade pattern

---
+++ Play3_Client_Side/Design Patterns/Facade/GunMagazines.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play3_Client_Side.DessignPatterns.Facade
+{
+    class GunMagazines
+    {
+        public void setGunMagazine()
+        {
+            Console.WriteLine("Changing magazine to Extended Mag");
+        }
+    }
+}
