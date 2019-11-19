46d0a48385df95d2b0e3f546888f29eea3708c80
Karolis <Karolis@email.com>
2019-11-19 08:35:59 +0200

    sql adapter added

---
+++ Play3_Client_Side/API/SqlProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+
+namespace Play3_Client_Side.API
+{
+    class SqlProvider
+    {
+        private string connectionString;
+        private SqlConnection connection;
+        private SqlDataAdapter adapter;
+
+        public SqlProvider()
+        {
+            connectionString = "";
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+        }
+
+        public void Insert(string sql)
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            adapter.InsertCommand = new SqlCommand(sql, connection);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            connection.Close();
+        }
+
+        public void Delete(string sql)
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            adapter.DeleteCommand = new SqlCommand(sql, connection);
+            adapter.DeleteCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            connection.Close();
+        }
+    }
+}
---
+++ Play3_Client_Side/Adapter/SqlAdapter.cs
@@ -0,0 +1,34 @@
+﻿using Play3_Client_Side.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play3_Client_Side.Adapter
+{
+    class SqlAdapter : IUpdater
+    {
+        SqlProvider sqlProvider = new SqlProvider();
+
+        public void DeleteData(string sql, string uuid = null, Action<string> onSuccess = null, Action<string> onFailure = null)
+        {
+            sqlProvider.Delete(sql);
+        }
+
+        public Task LoadData(string sql, Action<string> onSuccess = null, Action<string> onFailure = null)
+        {
+            Action action = () =>
+            {
+                Console.WriteLine("Not implemented yet.");
+            };
+
+            return new Task(action);
+        }
+
+        public void PostData(string sql, Dictionary<string, string> data = null, Action<string> onSuccess = null, Action<string> onFailure = null)
+        {
+            sqlProvider.Insert(sql);
+        }
+    }
+}
